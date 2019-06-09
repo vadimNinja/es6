1b79c61cbd7365992fa4a64336d57f29171e664a
vadimNinja <vs007@meta.ua>
2019-06-09 12:38:07 +0300

    change 1

--- jvs/index.js
+++ jvs/index.js
@@ -8,24 +8,24 @@ var car = {
 console.log(car);
 
 
-// function Car(name, year) {
-//     this.name = name;
-//     this.year = year
-// }
-//
-// Car.prototype.getAge = function () {
-//     return new Date().getFullYear() - this.year
-// };
-//
-// Car.prototype.color = 'black';
-//
-// var ford = new Car('Ford', 2015);
-// var bmw = new Car('BMW', 2017);
-//
-// ford.color = 'red';
-//
-// console.log(ford);
-// console.log(bmw);
+function Car(name, year) {
+    this.name = name;
+    this.year = year
+}
+
+Car.prototype.getAge = function () {
+    return new Date().getFullYear() - this.year
+};
+
+Car.prototype.color = 'black';
+
+var ford = new Car('Ford', 2015);
+var bmw = new Car('BMW', 2017);
+
+ford.color = 'red';
+
+console.log(ford);
+console.log(bmw);
 
 
 // var ford = Object.create({

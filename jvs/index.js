c0cc3a3815f8ff707254b1404e1442b1cf7ca215
vadimNinja <vs007@meta.ua>
2019-06-13 21:00:19 +0300

    modified:   jvs/index.js

--- jvs/index.js
+++ jvs/index.js
@@ -28,59 +28,65 @@ var bike = {
 };
 console.log(car);
 
-// var ford = Object.create({
-//     calculatedDistancePerYear: function () {
-//         Object.defineProperty(this, 'distancePerYear', {
-//             value: Math.ceil(this.distance / this.age),
-//             enumerable: false,
-//             writable: false,
-//             configurable: false
-//         })
-//     }
-// }, {
-//     name: {
-//         value: 'Ford',
-//         enumerable: true,
-//         writable: false,
-//         configurable: false
-//     },
-//     model: {
-//         value: 'Focus',
-//         enumerable: true,
-//         writable: false,
-//         configurable: false
-//     },
-//     year: {
-//         value: 2015,
-//         enumerable: true,
-//         writable: false,
-//         configurable: false
-//     },
-//     distance: {
-//         value: 12050,
-//         enumerable: true,
-//         writable: true,
-//         configurable: false
-//     },
-//     age: {
-//         enumerable: true,
-//         get: function () {
-//             return new Date().getFullYear() - this.year
-//         },
-//         set: function () {
-//
-//         }
-//     }
-// });
-//
-// ford.calculatedDistancePerYear();
-//
-// for (var key in ford) {
-//     if (ford.hasOwnProperty(key)) {
-//         console.log(key, ford[key])
-//     }
-// }
+var ford = Object.create({
+    calculatedDistancePerYear: function () {
+        Object.defineProperty(this, 'distancePerYear', {
+            value: Math.ceil(this.distance / this.age),
+            enumerable: false,
+            writable: false,
+            configurable: false
+        })
+    }
+}, {
+    name: {
+        value: 'Ford',
+        enumerable: true,
+        writable: false,
+        configurable: false
+    },
+    model: {
+        value: 'Focus',
+        enumerable: true,
+        writable: false,
+        configurable: false
+    },
+    year: {
+        value: 2015,
+        enumerable: true,
+        writable: false,
+        configurable: false
+    },
+    distance: {
+        value: 12050,
+        enumerable: true,
+        writable: true,
+        configurable: false
+    },
+    age: {
+        enumerable: true,
+        get: function () {
+            return new Date().getFullYear() - this.year
+        },
+        set: function () {
 
+        }
+    }
+});
+
+ford.calculatedDistancePerYear();
+
+for (var key in ford) {
+    if (ford.hasOwnProperty(key)) {
+        console.log(key, ford[key])
+    }
+}
+
+console.log(car);
+console.log(car);
+console.log(car);
+console.log(car);
+console.log(car);
+console.log(car);
 
 // var person = {
 //     name: 'Max',
